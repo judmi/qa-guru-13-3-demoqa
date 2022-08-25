74fcc32e06862cafd1041b2e5846174cb61f348b
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2022-08-25 17:55:53 +0300

    added page objects and components + test refactoring

---
+++ src/test/java/pages/RegistrationFormPage.java
@@ -0,0 +1,132 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+import pages.components.ResultsTableComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationFormPage {
+    CalendarComponent calendar = new CalendarComponent();
+    ResultsTableComponent resultsTable = new ResultsTableComponent();
+    SelenideElement firstNameInput = $("#firstName");
+
+
+    public RegistrationFormPage openPage(){
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        return this;
+    }
+
+    public RegistrationFormPage setFirstName(String value) { //RegistrationFormPage для возможности вызова цепочки методов
+        firstNameInput.setValue(value); // or $("[id=firstName]").setValue("Yulia");
+
+        return this;
+    }
+
+    public RegistrationFormPage setGender(String value) {
+        $("#genterWrapper").$(byText(value)).click(); //or $("#gender-radio-2").parent().click();
+
+        return this;
+    }
+
+    public RegistrationFormPage setLastName(String value) {
+        $("#lastName").setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setEmail(String value) {
+        $("#userEmail").setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setNumber(String value) {
+        $("#userNumber").setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setDateOfBirth(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        calendar.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationFormPage setDateOfBirthWithKeys(String day, String month, String year) {
+        $("#dateOfBirthInput").sendKeys(day + " " + month + " " + year);
+
+        return this;
+    }
+
+    public RegistrationFormPage checkResult(String key, String value) {
+        resultsTable.checkResult(key, value);
+
+        return this;
+    }
+
+    public RegistrationFormPage checkResultArr(String key, String[] value) {
+        resultsTable.checkResultArr(key, value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setSubjects(String[] subjects) {
+        for (int i = 0; i < subjects.length; i++) {
+            $("#subjectsInput").sendKeys(subjects[i]);
+            $("#subjectsInput").pressEnter();
+        }
+
+        return this;
+    }
+
+    public RegistrationFormPage setHobbies(String[] hobbies) {
+        for (int i = 0; i < hobbies.length; i++) {
+            $("#hobbiesWrapper").$(byText(hobbies[i])).click();
+        }
+
+        return this;
+    }
+
+    public RegistrationFormPage uploadPicture(String fileName) {
+        $("#uploadPicture").uploadFromClasspath(fileName);
+
+        return this;
+    }
+
+    public RegistrationFormPage setAddress(String value) {
+        $("#currentAddress").click();
+        $("#currentAddress").setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setState(String value) {
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Rajasthan")).click();
+
+        return this;
+    }
+
+    public RegistrationFormPage setCity(String value) {
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Jaipur")).click();
+
+        return this;
+    }
+
+    public RegistrationFormPage clickSubmit() {
+        $("#submit").click();
+
+        return this;
+    }
+
+}
---
+++ src/test/java/tests/StudentRegistrationFormTestWithPageObjects.java
@@ -0,0 +1,62 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class StudentRegistrationFormTestWithPageObjects extends BaseTest {
+
+    @Test
+    void formTest() {
+        String firstName = "Yulia";
+        String lastName = "Dmitrieva";
+        String email = "yulia.dmitrieva@yahoo.com";
+        String number = "1234567890";
+        String fileName = "test.jpg";
+        String address = "Test address";
+        String day = "24";
+        String month = "October";
+        String year = "1994";
+        String gender = "Female";
+        String state = "Rajasthan";
+        String city = "Jaipur";
+        String[] subjects = {"Maths", "Physics", "Computer Science"};
+        String[] hobbies = {"Sports", "Music"};
+
+        registrationFormPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setNumber(number);
+
+
+        registrationFormPage.setDateOfBirth("24", "October", "1994");
+
+        registrationFormPage.setSubjects(subjects)
+                .setHobbies(hobbies);
+
+        registrationFormPage.uploadPicture(fileName);
+
+        registrationFormPage.setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .clickSubmit();
+
+        registrationFormPage
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", number)
+                .checkResult("Date of Birth", day + " " + month + "," + year)
+                .checkResultArr("Subjects", subjects)
+                .checkResultArr("Hobbies", hobbies)
+                .checkResult("Picture", fileName).checkResult("Address", address)
+                .checkResult("State and City", state + " " + city);
+    }
+
+
+}
