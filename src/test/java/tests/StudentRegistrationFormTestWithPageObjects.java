7f107cb18caefd97767f9bd5b939c7b98b6921ba
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-02-21 21:29:42 +0300

    added demoqa tag

--- src/test/java/tests/StudentRegistrationFormTestWithPageObjects.java
+++ src/test/java/tests/StudentRegistrationFormTestWithPageObjects.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,6 +12,7 @@ import static io.qameta.allure.Allure.step;
 public class StudentRegistrationFormTestWithPageObjects extends BaseTest {
 
     @Test
+    @Tag("demoqa")
     void formTest() {
 
         String firstName = "Yulia";
