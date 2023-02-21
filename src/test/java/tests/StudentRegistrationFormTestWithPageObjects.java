4312bd6b7428c1c8d6bccbcf84ed473a56bb63f9
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-02-21 21:07:26 +0300

    added allure reports

--- src/test/java/tests/StudentRegistrationFormTestWithPageObjects.java
+++ src/test/java/tests/StudentRegistrationFormTestWithPageObjects.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 
 public class StudentRegistrationFormTestWithPageObjects extends BaseTest {
 
     @Test
     void formTest() {
+
         String firstName = "Yulia";
         String lastName = "Dmitrieva";
         String email = "yulia.dmitrieva@yahoo.com";
@@ -26,37 +28,40 @@ public class StudentRegistrationFormTestWithPageObjects extends BaseTest {
         String[] subjects = {"Maths", "Physics", "Computer Science"};
         String[] hobbies = {"Sports", "Music"};
 
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setNumber(number);
+        step("Fill form", () -> {
+            registrationFormPage.openPage()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setNumber(number);
 
 
-        registrationFormPage.setDateOfBirth(day, month, year);
+            registrationFormPage.setDateOfBirth(day, month, year);
 
-        registrationFormPage.setSubjects(subjects)
-                .setHobbies(hobbies);
+            registrationFormPage.setSubjects(subjects)
+                    .setHobbies(hobbies);
 
-        registrationFormPage.uploadPicture(fileName);
+            registrationFormPage.uploadPicture(fileName);
 
-        registrationFormPage.setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .clickSubmit();
+            registrationFormPage.setAddress(address)
+                    .setState(state)
+                    .setCity(city)
+                    .clickSubmit();
+        });
 
-        registrationFormPage
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", email)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", number)
-                .checkResult("Date of Birth", day + " " + month + "," + year)
-                .checkResultArr("Subjects", subjects)
-                .checkResultArr("Hobbies", hobbies)
-                .checkResult("Picture", fileName).checkResult("Address", address)
-                .checkResult("State and City", state + " " + city);
+        step("Check form results", () -> {
+            registrationFormPage
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", number)
+                    .checkResult("Date of Birth", day + " " + month + "," + year)
+                    .checkResultArr("Subjects", subjects)
+                    .checkResultArr("Hobbies", hobbies)
+                    .checkResult("Picture", fileName).checkResult("Address", address)
+                    .checkResult("State and City", state + " " + city);
+        });
     }
-
 
 }
