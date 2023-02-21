aba7fb90358d48846ea24177217d47ae9594d01b
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-02-21 21:25:49 +0300

    added properties tests + attach class

---
+++ src/test/java/tests/SystemPropertiesTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTest {
+
+    @Test
+    @Tag("properties")
+    void simplePropertyTest() {
+        String browserName = System.getProperty("browser_name", "firefox");
+        String browserVersion = System.getProperty("browser_version", "101");
+        String browserSize = System.getProperty("browser_size", "1920x1080");
+
+        System.out.println(browserName);
+        System.out.println(browserVersion);
+        System.out.println(browserSize);
+    }
+
+    @Test
+    @Tag("hello")
+    void simplePropertyTes5() {
+        System.out.println("Hello, " + System.getProperty("some_text", "qa.guru"));
+    }
+
+}
