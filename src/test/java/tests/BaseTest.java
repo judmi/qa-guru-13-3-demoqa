c4a2f536ddd728fbee361e81273f5ed418d92ddb
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-02-28 18:01:06 +0300

    changed browser size

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -21,7 +21,7 @@ public class BaseTest {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1024x720";
+        Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
